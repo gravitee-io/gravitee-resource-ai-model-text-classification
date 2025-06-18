db41f050b46237e9b28bc2ba82dcef2445ebc11a
Julien Giovaresco <dev@giovaresco.fr>
2025-06-18 08:27:03 +0200

    fix: download models in GRAVITEE_HOME/models directory

--- src/main/java/io/gravitee/resource/ai_model/TextClassificationAiModelResource.java
+++ src/main/java/io/gravitee/resource/ai_model/TextClassificationAiModelResource.java
@@ -27,6 +27,8 @@ import io.gravitee.resource.ai_model.model.ModelFile;
 import io.gravitee.resource.ai_model.model.ModelFileType;
 import io.reactivex.rxjava3.core.Single;
 import io.vertx.rxjava3.core.Vertx;
+import java.io.IOException;
+import java.nio.file.FileAlreadyExistsException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.List;
@@ -40,6 +42,9 @@ import org.springframework.context.ApplicationContextAware;
 public class TextClassificationAiModelResource
     extends AiTextClassificationModelResource<TextClassificationAiModelConfiguration>
     implements ApplicationContextAware {
+
+    private static final String GRAVITEE_HOME = "gravitee.home";
+    private static final String GRAVITEE_HOME_PATH = System.getProperty(GRAVITEE_HOME);
 
     private String modelId;
     private List<ModelFile> modelFiles;
@@ -57,7 +62,7 @@ public class TextClassificationAiModelResource
         var modelName = configuration().model().modelName();
         this.modelId = modelName;
         this.modelFiles = getModelFiles();
-        this.modelDirectory = Files.createTempDirectory(modelName.replace("/", "-"));
+        this.modelDirectory = getFileDirectory();
 
         this.vertx = applicationContext.getBean(Vertx.class);
         this.inferenceServiceClient = new InferenceServiceClient(vertx);
@@ -74,6 +79,19 @@ public class TextClassificationAiModelResource
             new ModelFile(modelConfig.tokenizerFile(), ModelFileType.TOKENIZER),
             new ModelFile(modelConfig.configFile(), ModelFileType.CONFIG)
         );
+    }
+
+    private Path getFileDirectory() throws IOException {
+        Path directory = Path.of(GRAVITEE_HOME_PATH + "/models/" + modelId);
+        try {
+            return Files.createDirectories(directory);
+        } catch (FileAlreadyExistsException faee) {
+            log.debug("{} already exists, skip directory creation", directory);
+            return directory;
+        } catch (Exception e) {
+            log.warn("Failed to create models directory, creating temp directory", e);
+            return Files.createTempDirectory(modelId.replace("/", "-"));
+        }
     }
 
     @Override
